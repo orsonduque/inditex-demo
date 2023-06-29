8491b25920ca4b05ea13fc57b345d41f51844b0a
“orsonduque” <orsonduque@gmail.com>
2023-06-29 08:54:47 -0400

    Initial version of inditex generated by generator-jhipster@7.9.2

---
+++ src/test/java/com/neoris/inditex/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.neoris.inditex;
+
+import com.neoris.inditex.InditexApp;
+import com.neoris.inditex.config.AsyncSyncConfiguration;
+import com.neoris.inditex.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { InditexApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
